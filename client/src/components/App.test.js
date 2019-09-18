d92d6e5d6b5c6f14b622d16bde1b5a33c9181797
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-09-18 16:08:40 +0530

    chore: set up tests

---
+++ client/src/components/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import App from './App';
+import Header from './Header/Header';
+
+describe('<App />', () => {
+  const wrapper = mount(<App />);
+  it('should render', () => {
+    wrapper.debug();
+  });
+  it('should contain the navbar and there should be only one navbar', () => {
+    const headerWrapper = wrapper.find('Header');
+    expect(headerWrapper.length).toBe(1);
+  });
+});
